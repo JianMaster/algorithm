e19741ba8d24e1d62b65d05617fa35db3d3093d7
JianMaster <1447247475@qq.com>
2023-12-02 16:03:23 +0800

    lz77

--- 压缩算法/LZ77.cs
+++ 压缩算法/LZ77.cs
@@ -3,37 +3,60 @@ using System;
 [Serializable]
 struct DataBlock
 {
-    byte offset;
-    byte length;
-    char c;
+    public DataBlock(byte offset, byte length, byte b)
+    {
+        this.offset = offset;
+        this.length = length;
+        this.b = b;
+    }
+    public byte offset;
+    public byte length;
+    public byte b;
 }
 
-public class LZ77 {
-    private bool CheckIsMatch(byte[] buffer, int pl, int pr, int bl, int br)
+public class LZ77
+{
+    public static (int, int) Match(byte[] buffer, int pl, int windowLength, int pr, int bufferLength)
     {
-        while()
-    }
-    private static (int offset,int length) Match(byte[] buffer,int pl,int pr,int bl,int br)
-    {
-        for(int i = br; i > bl; i--)
+        int start = pl, maxLength = 1, endIdx = pl + windowLength;
+        for (int i = pl; i < endIdx; i++)
         {
-            
+            int length = 0;
+            for (int j = 0; j < bufferLength; j++)
+            {
+                if (i + j == endIdx)
+                {
+                    break;
+                }
+                if (buffer[i + j] != buffer[pr + j])
+                {
+                    break;
+                }
+                length++;
+            }
+            if (length > maxLength)
+            {
+                maxLength = length;
+                start = i;
+            }
 
         }
+        return (start, maxLength);
     }
-    public static void Compress(int windowLength, int bufferLength, string inPath, string outPath)
+    public static void Compress(byte windowLength, byte bufferLength, string inPath, string outPath)
     {
-        if(File.Exists(inPath))
+        if (File.Exists(inPath))
         {
-            using(FileStream fs = new FileStream(inPath, FileMode.Open, FileAccess.Read))
+            byte[] newBuffer;
+            using (FileStream fs = new FileStream(inPath, FileMode.Open, FileAccess.Read))
             {
-                //缓冲区应该不大于窗口大小，大于也没意义
-                if(bufferLength > windowLength)
+                if (bufferLength > windowLength)
                 {
                     bufferLength = windowLength;
                 }
-                byte[] bufffer = new byte[fs.Length+windowLength];
-                fs.Read(bufffer, windowLength, (int)fs.Length);
+                byte[] buffer = new byte[fs.Length + windowLength];
+                //缓冲区应该不大于窗口大小，大于也没意义
+                fs.Read(buffer, windowLength, (int)fs.Length);
                 //前向缓冲区指针
                 int preBufferL = windowLength;
                 int preBufferR = windowLength + bufferLength - 1;
@@ -43,8 +66,36 @@ public class LZ77 {
                 int prtR = windowLength - 1;
 
                 List<DataBlock> list = new List<DataBlock>();
+                while (preBufferR < buffer.Length)
+                {
+                    (int start, int length) = Match(buffer, ptrL, windowLength, preBufferL, bufferLength);
+                    if (length != 1)
+                    {
+                        list.Add(new DataBlock((byte)(start - ptrL), (byte)length, buffer[preBufferL + length]));
+                        length += 1;
+                    }
+                    else
+                    {
+                        list.Add(new DataBlock(0, 0, buffer[preBufferL]));
+                    }
+                    ptrL += length;
+                    prtR += length;
+                    preBufferL += length;
+                    preBufferR += length;
 
+                }
 
+                newBuffer = new byte[list.Count * 3];
+                for (int i = 0; i < list.Count; i++)
+                {
+                    newBuffer[i * 3] = list[i].offset;
+                    newBuffer[i * 3 + 1] = list[i].length;
+                    newBuffer[i * 3 + 2] = list[i].b;
+                }
+            }
+
+            using (FileStream fs = new FileStream(outPath, FileMode.OpenOrCreate, FileAccess.Write)){
+                fs.Write(newBuffer, 0, newBuffer.Length);
             }
         }
     }
--- 压缩算法/Program.cs
+++ 压缩算法/Program.cs
@@ -12,6 +12,33 @@ namespace ProgramName
     }
     public class Program
     {
+        private static (int, int) Match(byte[] buffer, int pl, int windowLength, int pr, int bufferLength)
+    {
+        int start = pl, maxLength = 1, endIdx = pl + windowLength;
+        for (int i = pl; i < endIdx; i++)
+        {
+            int length = 0;
+            for (int j = 0; j < bufferLength; j++)
+            {
+                if (i + j == endIdx)
+                {
+                    break;
+                }
+                if (buffer[i + j] != buffer[pr + j])
+                {
+                    break;
+                }
+                length++;
+            }
+            if (length > maxLength)
+            {
+                maxLength = length;
+                start = i;
+            }
+
+        }
+        return (start, maxLength);
+    }
         public static void Main(string[] args)
         {
             
@@ -22,6 +49,14 @@ namespace ProgramName
             Console.WriteLine(nameof(ProgramName.A));
 
             Console.WriteLine(ba is A);
+
+            byte[] buffer = new byte[12]{1,2,3,1,2,5,  1,2,5,3,4,5};
+            (int start, int length) = LZ77.Match(buffer, 0, 6, 6, 3);
+            Console.WriteLine(start);
+            Console.WriteLine(length);
+
+            LZ77.Compress(100, 100, "D:\\Users\\PC5080\\Desktop\\gitProject\\algorithm\\压缩算法\\aa.txt", "D:\\Users\\PC5080\\Desktop\\gitProject\\algorithm\\压缩算法\\aa.lz77");
         }
     }
+    
 }
